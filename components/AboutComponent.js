042a9d457de518f03c7956053b8c5ad5ce3dd412
Scott Sutton <scosut@hotmail.com>
2021-03-18 15:52:19 -0700

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -34,16 +35,29 @@ class About extends Component {
           leftAvatar={{ source: { uri: baseUrl + item.image } }} />
       );
     };
-
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Mission />
+            <Card title="Community Partners">
+              <Text>{this.props.partners.errMess}</Text>
+            </Card>
+          </Animatable.View>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
-        <Mission />
-        <Card title="Community Partners">
-          <FlatList
-            data={this.props.partners.partners}
-            renderItem={renderPartner}
-            keyExtractor={item => item.id.toString()} />
-        </Card>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Mission />
+          <Card title="Community Partners">
+            <FlatList
+              data={this.props.partners.partners}
+              renderItem={renderPartner}
+              keyExtractor={item => item.id.toString()} />
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -3,6 +3,7 @@ import { FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
   return {
@@ -19,12 +20,14 @@ class Directory extends Component {
     const { navigate } = this.props.navigation;
     const renderDirectoryItem = ({ item }) => {
       return (
-        <Tile
-          title={item.name}
-          caption={item.description}
-          featured
-          onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-          imageSrc={{ uri: baseUrl + item.image }} />
+        <Animatable.View animation="fadeInRightBig" duration={2000}>
+          <Tile
+            title={item.name}
+            caption={item.description}
+            featured
+            onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+            imageSrc={{ uri: baseUrl + item.image }} />
+        </Animatable.View>
       );
     };
 
