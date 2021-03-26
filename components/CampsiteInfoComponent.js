0b1b8d8d60b649774fc0dbe099b4dd484cf8765a
Scott Sutton <scosut@hotmail.com>
2021-03-26 08:47:47 -0700

    Secure Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -52,6 +52,7 @@ function RenderCampsite(props) {
   const view = React.createRef();
 
   const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+  const recognizeComment = ({ dx }) => (dx > 200) ? true : false;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -79,6 +80,9 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
+      }
+      else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
       return true;
     }
